a4403f57a806eb67966956504794579c11ecd5c6
FangZurui <1025448722@qq.com>
2022-04-07 18:52:19 +0800

    first commit

---
+++ Code_Python/Python_Single/drawscatter.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import numpy as np
+
+filename="E:\CodeField1\Code_Python\Python_Single\weather.csv"
+
+#从文件中获取数值
+data=pd.read_csv(filename,encoding="utf-8")
+data=np.array(data)
+print(data)
+
+times,obv1s,obv2s=[],[],[]
+#提取表格中每一列数据
+for row in data:
+    time=row[0]
+    times.append(time)
+    obv1=row[1]
+    obv1s.append(obv1)
+    obv2=row[2]
+    obv2s.append(obv2)
+
+#绘制折线图
+fig=plt.figure(dpi=128,figsize=(8,6))           #初始化figure窗口，可绘制多条曲线
+plt.plot(times,obv1s)
+plt.plot(times,obv2s)
+
+plt.show()
